03d002126d410bcee245ec4efd15c86fe3f56d32
sap19 <stevenportillo19@hotmail.com>
2020-02-04 10:25:27 -0500

    Completed
    Worked a little on the css,
    Check Boxes work, and removing items, also local storage.

--- js/main.js
+++ js/main.js
@@ -2,27 +2,51 @@ let app = new Vue({
     el: "#vueApp",
     data: {
         list:[
-            {id:1, item:'test', checked:true}
+            {id:1, item:'test', checked:false}
         ],
-        checkedId:[],
+        label:[
+            {text:'Not complete'}
+        ],
         welcomeMessage: '',
         newItem: '',
-        checked: '',
+        checked: false,
     },
-    methods: {
+    methods: 
+    {
         AddingItem:function()
         {
             let id = this.list.lenght + 1
             if (this.newItem !== '')
             {
-                const newList = {id:id,item:this.newItem}
+                const newList = {id:id,item:this.newItem, checked:false}
                 this.list.push(newList)
+                this.Save()
             }
         },
-       
-       
+       removeItem(index)
+       {
+           this.list.splice(index,1)
+           this.Save();
+       },
+       Completed:function()
+       {
+           app.list.checked = !app.checked
+           app.label.text = app.checked ? 'Not complete' : 'Complete';
+       },
+       Save:function()
+        {
+        const parsed = JSON.stringify(this.list);
+        localStorage.setItem('list', parsed);
+        }
     },
     mounted() {
-
-    },
+        if (localStorage.getItem('list')) {
+          try {
+            this.list = JSON.parse(localStorage.getItem('list'));
+          } catch(e) {
+            localStorage.removeItem('list');
+          }
+        }
+      },
+   
 });
