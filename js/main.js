f65161a3443ca04ddc89f60ca4a303c98f25ba03
sap19 <stevenportillo19@hotmail.com>
2020-02-03 19:53:36 -0500

    List adding
    Now able to push things to a list

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 let app = new Vue({
     el: "#vueApp",
     data: {
-        welcomeMessage: ''
+        list:[],
+        welcomeMessage: '',
+        newItem: ''
     },
     methods: {
-
+        AddingItem:function()
+        {
+            let id = this.list.lenght + 1
+            if (this.newItem !== '')
+            {
+                const newList = {id:id,item:this.newItem, checkbox:this.checkbox}
+                this.list.push(newList)
+            }
+        }
     },
     mounted() {
 
